285fb09ee5be39ff40215573ed6b2b8cfe3f7eb5
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2023-02-14 00:18:06 +0530

    Basic CSV reading

---
+++ read_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+import pdb
+
+# Be careful while joining path because it's OS specific
+df = pd.read_csv(os.path.join(os.getcwd()+r'\dataset\survey_results_public.csv'))
+
+# pd.set_option('display.max_columns',85) mainly used in jupyter notebook
+# print(df)
+
+print(df.shape) #size of dataframe in (row, column) format
+info=df.info() #Return metadata info about dataframe
+print(info)
+
+print(df.head(7)) #head(<n>) returns first n rows default is 5
+print(df.tail()) #tail(<n>) returns last n rows default is 5
